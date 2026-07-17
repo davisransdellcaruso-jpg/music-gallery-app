a1183ae5402d7c37184882adad2d7c01e7d8a94c
Davis Ransdell Caruso <davisransdellcaruso-1695@users.noreply.vercel.com>
2026-07-17 08:59:56 -0400

    Add Reddit pixel tracking

--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from "next/document";
+import Script from "next/script";
 
 export default function Document() {
   return (
@@ -7,6 +8,15 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <NextScript />
+
+        {/* Reddit Pixel */}
+        <Script id="reddit-pixel" strategy="afterInteractive">
+          {`
+            !function(w,d){if(!w.rdt){var p=w.rdt=function(){p.sendEvent?p.sendEvent.apply(p,arguments):p.callQueue.push(arguments)};p.callQueue=[];var t=d.createElement("script");t.src="https://www.redditstatic.com/ads/pixel.js?pixel_id=a2_dlt528w43ns9",t.async=!0;var s=d.getElementsByTagName("script")[0];s.parentNode.insertBefore(t,s)}}(window,document);
+            rdt('init','a2_dlt528w43ns9');
+            rdt('track', 'PageVisit');
+          `}
+        </Script>
       </body>
     </Html>
   );
